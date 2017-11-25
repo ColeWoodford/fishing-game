de61968cc8ada71a0e50259b4ba6a7169dd739bc
Cole <woodford.cole@gmail.com>
2017-11-25 13:00:03 -0700

    Logic for fishing minigame is finished.

--- Assets/Scripts/MarkerController.cs
+++ Assets/Scripts/MarkerController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MarkerController : MonoBehaviour {
 
-	private Rigidbody2D markerBody;
 	private bool stop;
 	private Vector3 pos1 = new Vector3 (-8.7f, -3.51f, 0);
 	private Vector3 pos2 = new Vector3 (1.71f, -3.51f, 0);
@@ -12,13 +11,16 @@ public class MarkerController : MonoBehaviour {
 	public float markerSpeed = 1.0f;
 
 	void Start () {
-		markerBody = GetComponent<Rigidbody2D>();
 		stop = false;
+	}
+
+	public bool getStop()
+	{
+		return stop;
 	}
 
 	void Update () {
 		MoveCursor (stop);
-		//MyInput ();
 	}
 
 	void MoveCursor(bool s)
@@ -27,19 +29,8 @@ public class MarkerController : MonoBehaviour {
 			transform.localPosition = Vector3.Lerp (pos1, pos2, (Mathf.Sin (markerSpeed * Time.time) + 1.0f) / 2.0f);
 		}
 		if (Input.GetKeyDown ("space")) {
-			print ("space key was pressed.");
+			//print ("space key was pressed.");
 			stop = true;
-			//print (markerBody.name);
-			//transform.localPosition = Vector3.zero;
-		}
-	}
-
-	void MyInput()
-	{
-		if (Input.GetKeyDown ("space")) {
-			print ("space key was pressed.");
-			print (markerBody.name);
-			transform.localPosition = Vector3.zero;
 		}
 	}
 }
---
+++ Assets/Scripts/TargetController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour {
+
+	public MarkerController mark;
+	private Vector3 pos;
+	private bool win = false;
+
+	// Use this for initialization
+	void Start () {
+		pos = new Vector3(Random.Range(-8.7f,1.71f),-3.51f,0);
+		transform.localPosition = pos;
+	}
+
+	void Update() {
+		if (mark.getStop ()) {
+			WonGame (win);
+			enabled = false;
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.CompareTag ("MarkerTag")) {
+			win = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		if(other.CompareTag("MarkerTag")) {
+			win = false;
+		}
+	}
+
+	void WonGame(bool w) {
+		if (w) {
+			print ("YOU WIN!");
+		} else {
+			print ("You lose.");
+		}
+	}
+}
